c808257a2f6519288babd3e52634c5df45701ead
Andre Lins <andrelins2403@hotmail.com>
2024-12-03 10:09:57 -0300

    first commit

---
+++ src/main/java/org/focorh/sistemarh/SistemarhApplication.java
@@ -0,0 +1,13 @@
+package org.focorh.sistemarh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemarhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemarhApplication.class, args);
+	}
+
+}
